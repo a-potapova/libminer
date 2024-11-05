be63163b37b51d8ce8ffe6473bd2a9cde6af5a92
a-potapova <annaa.potapova@gmail.com>
2024-11-05 10:35:18 -0800

    documentation update

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,14 +1,28 @@
 #' Provide Number of R packages by library
+#' @param sizes Should sizes of libraries be calculated. Default False
 #'
 #' @return a data.frame of R packages by library
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function(){
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)) {
+    stop("'sizes' must be logical (TRUE or FALSE)")
+  }
   pkgs <- utils::installed.packages()
-  pkg_tbl <- table(pkgs[,"LibPath"])
+  pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
-  names(pkg_df) <- c("library","n_packages")
+  names(pkg_df) <- c("Library", "n_packages")
+
+  if (isTRUE(sizes)) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
   pkg_df
 }
